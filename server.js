71d2df9f74ab3279f73353b8c3b4d5de793b6298
emanuele zanni <emanuele zanni>
2017-04-11 15:59:44 +0200

    First Commit

---
+++ server.js
@@ -0,0 +1,20 @@
+'use strict';
+const express = require('express');
+const app = new express();
+var x = 0;
+
+app.listen(8000, () =>{
+	console.log("Server in ascolto sulla porta 8000");
+});
+
+app.get('/', (req,res) => {
+	res.send('Homepage');
+});
+
+app.get('/Author', (req,res) => {
+	res.send('Site created by Zanni');
+});
+
+app.get('*', (req,res) => {
+	res.status(404).send('Hic sunt leones! (404)');
+});
